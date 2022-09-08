50681d2568058f0536aacd6ec521dc2717ef5cfa
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-09-08 23:41:33 +0300

    Adding new model attribute book

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.Bookstore.domain.Book;
+
 @Controller
 public class BookController {
 
 	@GetMapping("/index")
 
 	public String index(Model model) {
-
+		model.addAttribute("book", new Book()); 
 		return "/index";
 	}
 }
