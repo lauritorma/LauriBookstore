e94e06645b5b2591c5820c23883a5775c5f02204
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-09-12 17:52:14 +0300

    Created booklist view, added controller

---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,20 @@
+package com.example.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.Bookstore.domain.BookRepository;
+
+@Controller
+public class BookController {
+		@Autowired
+		private BookRepository repository;
+	
+	@GetMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+}
