24b8396820ba14e98197bae969cf3321e900e1bb
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-09-12 16:27:27 +0300

    Added JPA & H2, created entity Book, added two demo lines to database, updated application properties and deleted BookController for now.

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,32 @@
 package com.example.Bookstore;
 
+
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book a = new Book("Harry Potter","J.K Rowling", "20", "12345", "2021");
+			Book b = new Book("Koiramäki", "Mauri Kunnas", "20", "951114399", "1996" );
+			
+			repository.save(a);
+			repository.save(b);
+	 
+	};
 	}
 
 }
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,50 +1,80 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
-	private String title;
-	private String author;
-	private long year;
-	private long isbn;
-	private long price;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String title,author,yearPublished,isbn,price;
+	
+	
+	public Book() {}
+	
 
-	public String getTitle() {
-		return title;
-	}
+		public Book(String title, String author,String yearPublished, String isbn, String price) {
+			super();
+			this.title = title;
+			this.author = author;
+			this.yearPublished = yearPublished;
+			this.isbn = isbn;
+			this.price = price;
+			
+		}
+	
+	
+	
+		public String getYearPublished() {
+			return yearPublished;
+		}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
-	public String getAuthor() {
-		return author;
-	}
+		public void setYearPublished(String yearPublished) {
+			this.yearPublished = yearPublished;
+		}
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
 
-	public long getYear() {
-		return year;
-	}
+		public Long getId() {
+			return id;
+			
+			
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		
+		public String getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+		public String getPrice() {
+			return price;
+		}
+		public void setPrice(String price) {
+			this.price = price;
+		}
+	
+	
 
-	public void setYear(long year) {
-		this.year = year;
-	}
-
-	public long getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(long isbn) {
-		this.isbn = isbn;
-	}
-
-	public long getPrice() {
-		return price;
-	}
-
-	public void setPrice(long price) {
-		this.price = price;
-	}
+	
+	
 }
