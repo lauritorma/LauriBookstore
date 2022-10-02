53c709a62ce1b72ffa1d6195abda54bef5edd661
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-10-02 21:26:00 +0300

    added spring security features, restricted DELETE functionality to admin role only

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,12 @@ public class BookController {
 		@Autowired
 		private CategoryRepository crepository;
 	
-		
+		// Show all books
+	@RequestMapping(value="/login")
+	 public String login() {	
+	 return "login";
+	    }	
+	    
 	//Show all books
 	@GetMapping("/booklist")
 	public String bookList(Model model) {
@@ -64,7 +70,9 @@ public class BookController {
     } 
     
     //Delete book
+    
     @GetMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
     	return "redirect:/booklist";
